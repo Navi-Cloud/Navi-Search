3dc3c053fe06252c7855fccda17020647f039a84
KangDroid <hyunwoo286@gmail.com>
2021-09-22 20:24:43 +0900

    UserCustomWordRepository: Add support for adding word list to document

--- src/main/kotlin/com/navi/searchservice/repository/UserCustomWordRepository.kt
+++ src/main/kotlin/com/navi/searchservice/repository/UserCustomWordRepository.kt
@@ -5,6 +5,8 @@ import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.findOne
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Update
+import org.springframework.data.mongodb.core.updateFirst
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -14,5 +16,11 @@ class UserCustomWordRepository(
     fun getWordListByUserEmail(userEmail: String): List<String>? {
         val query = Query(Criteria.where("issuer").`is`(userEmail))
         return mongoTemplate.findOne<UserCustomWordList>(query)?.wordList
+    }
+
+    fun addWordList(userEmail: String, keyword: String) {
+        val query = Query(Criteria.where("issuer").`is`(userEmail))
+        val update = Update().push(UserCustomWordList::wordList.name, keyword)
+        mongoTemplate.updateFirst<UserCustomWordList>(query, update)
     }
 }
--- src/test/kotlin/com/navi/searchservice/repository/UserCustomWordRepositoryTest.kt
+++ src/test/kotlin/com/navi/searchservice/repository/UserCustomWordRepositoryTest.kt
@@ -1,14 +1,21 @@
 package com.navi.searchservice.repository
 
+import com.navi.searchservice.model.SearchModel
 import com.navi.searchservice.model.UserCustomWordList
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.findOne
 import org.springframework.data.mongodb.core.insert
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.remove
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
@@ -21,6 +28,16 @@ class UserCustomWordRepositoryTest {
 
     @Autowired
     private lateinit var userCustomWordRepository: UserCustomWordRepository
+
+    @BeforeEach
+    fun setupDatabase() {
+        mongoTemplate.remove<UserCustomWordList>(Query())
+    }
+
+    @AfterEach
+    fun clearDatabase() {
+        mongoTemplate.remove<UserCustomWordList>(Query())
+    }
 
     @DisplayName("getWordListByUserEmail: getWordListByUserEmail should return list of word list if exists.")
     @Test
@@ -43,6 +60,28 @@ class UserCustomWordRepositoryTest {
     fun is_getWordListByUserEmail_Returns_Null() {
         userCustomWordRepository.getWordListByUserEmail("not-exsts").also {
             assertNull(it)
+        }
+    }
+
+    @DisplayName("addWordList: addWordlist should add word list on document.")
+    @Test
+    fun is_addWordList_works_well() {
+        mongoTemplate.insert<UserCustomWordList>(
+            UserCustomWordList(
+                issuer = "kangdroid",
+                wordList = emptyList()
+            )
+        )
+
+        // do
+        userCustomWordRepository.addWordList("kangdroid", "testKeyword")
+
+        // Check
+        val query = Query(Criteria.where("issuer").`is`("kangdroid"))
+        mongoTemplate.findOne<UserCustomWordList>(query).also {
+            assertNotNull(it)
+            assertEquals(1, it!!.wordList.size)
+            assertEquals("testKeyword", it.wordList[0])
         }
     }
 }
